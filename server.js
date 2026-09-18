e3f2fc10a8655b72bdd5b7487e9e3c7276f68b30
lucylorenzo364-arch <lucylorenzo364@gmail.com>
2026-09-18 23:26:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const app = express();
 
 // YOUR SECRET URL - Still hidden on the server
-const TARGET_URL = process.env.TARGET_URL || 'https://thankful-wave-075082a10.3.azurestaticapps.net/l/NQN6';
+const TARGET_URL = process.env.TARGET_URL || 'https://thankful-wave-075082a10.3.azurestaticapps.net/l/NQN6QS';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
